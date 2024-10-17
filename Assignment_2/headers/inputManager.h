b4e5dd0a204568818a1a6d98263a5af3ff4746ba
Cyese <quangtuelamnguyen2003@gmail.com>
2024-10-17 13:32:02 +0700

    Add input checker

--- Assignment_2/headers/inputManager.h
+++ Assignment_2/headers/inputManager.h
@@ -4,18 +4,19 @@
 #include"keyBinding.h"
 #include"Character.h"
 
-
-
 class InputManager {
 private:
-    Character ** p1_charater; 
-    Character ** p2_charater; 
+    Character ** p1_charater_list; 
+    Character ** p2_charater_list; 
 protected:
-    
+    Character * p1;
+    Character * p2;
+    void changePlayer(Character * player, PlayerAction action) const ;
+    void movePlayer(Character * player, PlayerAction action) const;
 public:
-    void input(SDL_Keycode key);
+    void input(SDL_Keycode key) const;
     InputManager(Character ** character_1, Character ** character_2);
 };
 
-extern InputManager inputManager;
 #endif
+extern InputManager inputManager;
---
+++ Assignment_2/src/inputManager.cpp
@@ -0,0 +1,58 @@
+#include "../headers/inputManager.h"
+
+InputManager inputManager;
+
+InputManager::InputManager(Character ** character_1, Character ** character_2) {
+    p1_charater = character_1;
+    p2_charater = character_2;
+}
+
+void InputManager::input(SDL_Keycode key) const {  
+    Actions action_mapped = keyBindingsInstance.getAction(key);
+    Character * character;
+    switch (action_mapped.character_num);
+    {
+    case 1: character = p1_charater; break;
+    case 2: character = p2_charater; break;
+    default: return;
+    }
+
+    switch (action_mapped.action)
+    {
+    case (PlayerAction::MoveUp):
+    case (PlayerAction::MoveDown):
+    case (PlayerAction::MoveLeft):
+    case (PlayerAction::MoveRight):
+        movePlayer(character, action_mapped.action);
+        break;
+    case (PlayerAction::Action1):
+    case (PlayerAction::Action2):
+    case (PlayerAction::Action3):
+        changePlayer(character, action_mapped.action);
+        break;
+    default:
+        return;
+    }
+}; 
+
+void InputManager::changePlayer(Character * player, PlayerAction action) const {
+    int char_num;
+    switch (action)
+    {
+    case (PlayerAction::Action1): char_num= 0 ;break;
+    case (PlayerAction::Action2): char_num= 1 ;break;
+    case (PlayerAction::Action3): char_num= 2 ;break;
+    default: return;
+    }
+}
+
+void InputManager::movePlayer(Character * player, PlayerAction action) const {
+    switch (action)
+    {
+    case (PlayerAction::MoveUp): player->applyAcc({0.0f, 1.0f}); break;
+    case (PlayerAction::MoveDown): player->applyAcc({0.0f, -1.0f}); break;
+    case (PlayerAction::MoveLeft): player->applyAcc({-1.0f, 0.0f}); break;
+    case (PlayerAction::MoveRight): player->applyAcc({1.0f, 0.0f}); break;
+    default: return;
+    }
+}
